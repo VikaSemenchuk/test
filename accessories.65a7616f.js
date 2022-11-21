4239a1d6ca09bbb4eeb241ec632c039307eccd6e
VikaSemenchuk <111804432+VikaSemenchuk@users.noreply.github.com>
2022-11-21 02:04:26 +0000

    Deploying to gh-pages from @ VikaSemenchuk/test@02df5148a5b4a214417d61991d5522b376a4287a 🚀

---
+++ accessories.65a7616f.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(()=>{const n="true"===e.getAttribute("aria-expended")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expended",!n),t.classList.toggle("is-open")}))})();
+//# sourceMappingURL=accessories.65a7616f.js.map
---
+++ accessories.b86f2294.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expended")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expended",!n),t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=accessories.b86f2294.js.map
