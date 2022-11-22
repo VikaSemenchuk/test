1a6ebb250b2be0d129af587cf414dbd58d452ad2
OksLu <112413121+OksLu@users.noreply.github.com>
2022-11-23 01:46:38 +0200

    /

--- src/index.js
+++ src/index.js
@@ -14,3 +14,28 @@
     body.classList.toggle('no-scroll');
   });
 })();
+
+{
+  /* <include src="./js/contact-modal.js"></include> */
+}
+
+// @import './js/contact-modal.js';
+
+{
+  /* <script src="./js/contact-modal.js"></script> */
+}
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-contactModal-open]'),
+    closeModalBtn: document.querySelector('[data-contactModal-close]'),
+    contactModal: document.querySelector('[data-contactModal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleContactModal);
+  refs.closeModalBtn.addEventListener('click', toggleContactModal);
+
+  function toggleModal() {
+    refs.Modal.classList.toggle('is-hidden');
+  }
+})();
