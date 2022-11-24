3e3e61691af57ddab4e0bcea4fba3c15780c133c
VikaSemenchuk <viktoriia.semenchuk@gmail.com>
2022-11-24 23:39:34 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './js/menu';
+import './js/menu2';
+
 import './js/swiper-slider';
 
-// import './js/contact-modal';
+import './js/contact-modal';
 
 import './js/take-order-add';
 import './js/take-order-buy';
